4bbb4ea64382b6b8b61509eae4b57aa046befc8a
Ryan Pedersen <ryanpedersen42@gmail.com>
2019-12-09 12:49:15 -0600

    adding timeout to login spec

--- cypress/integration/auth/signin_spec.js
+++ cypress/integration/auth/signin_spec.js
@@ -12,7 +12,8 @@ describe('Sign In Tests', () => {
 
   it('signs me out', () => {
     cy.get('.form-input[data-cy=sign-in-email]').type('a@a.com')
-    cy.get('.form-input[data-cy=sign-in-password]').type('Tester1!')
+    cy.get('.form-input[data-cy=sign-in-password]').type('Tester1!', { timeout: 40000 })
+    // cy.get('.custom-button[data-cy=sign-in-button]').click()
     cy.get('.option[data-cy=sign-out-header]').click()
     cy.get('.header').contains('SIGN IN') 
   })
