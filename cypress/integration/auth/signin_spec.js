4100d0d250f7a6eadc5c2d67fc7d491785caf1fe
Ryan Pedersen <ryanpedersen42@gmail.com>
2019-12-09 13:28:36 -0600

    new sign in and out test for circleci passing

--- cypress/integration/auth/signin_spec.js
+++ cypress/integration/auth/signin_spec.js
@@ -6,16 +6,10 @@ describe('Sign In Tests', () => {
   it('can sign me in and out', () => {
     cy.get('.form-input[data-cy=sign-in-email]').type('a@a.com')
     cy.get('.form-input[data-cy=sign-in-password]').type('Tester1!{enter}')
+    cy.wait(500)
     cy.url().should('eq', 'http://localhost:3000/')
     cy.get('.header').contains('SIGN OUT') 
     cy.get('.option[data-cy=sign-out-header]').click()
     cy.get('.header').contains('SIGN IN') 
   })
-
-  // it('signs me out', () => {
-  //   cy.get('.form-input[data-cy=sign-in-email]').type('a@a.com')
-  //   cy.get('.form-input[data-cy=sign-in-password]').type('Tester1!')
-  //   cy.get('.option[data-cy=sign-out-header]').click()
-  //   cy.get('.header').contains('SIGN IN') 
-  // })
 })
