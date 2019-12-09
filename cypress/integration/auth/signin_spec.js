376673acc82fe0a85059c8a59f6169c6a384d684
Ryan Pedersen <ryanpedersen42@gmail.com>
2019-12-09 13:01:09 -0600

    updating signout test

--- cypress/integration/auth/signin_spec.js
+++ cypress/integration/auth/signin_spec.js
@@ -13,10 +13,19 @@ describe('Sign In Tests', () => {
   it('signs me out', () => {
     cy.get('.form-input[data-cy=sign-in-email]').type('a@a.com')
     cy.get('.form-input[data-cy=sign-in-password]').type('Tester1!{enter}')
-    cy.wait(500)
-    // cy.get('.custom-button[data-cy=sign-in-button]').click()
+    cy.url().should('eq', 'http://localhost:3000/')
+    cy.get('.header').contains('SIGN OUT') 
     cy.get('.option[data-cy=sign-out-header]').click()
-    cy.wait(500)
     cy.get('.header').contains('SIGN IN') 
+
+
+    // cy.get('.form-input[data-cy=sign-in-email]').type('a@a.com')
+    // cy.wait(500)
+    // cy.get('.form-input[data-cy=sign-in-password]').type('Tester1!')
+    // cy.wait(500)
+    // cy.get('.custom-button[data-cy=sign-in-button]').click()
+    // cy.get('.option[data-cy=sign-out-header]').click()
+    // cy.wait(500)
+    // cy.get('.header').contains('SIGN IN') 
   })
 })
