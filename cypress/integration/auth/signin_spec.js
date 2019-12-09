f55d2b853b7a41b94ae511b0f64d2a1a4d7bf6f5
Ryan Pedersen <ryanpedersen42@gmail.com>
2019-12-09 13:20:59 -0600

    trying new test for CI

--- cypress/integration/auth/signin_spec.js
+++ cypress/integration/auth/signin_spec.js
@@ -3,29 +3,19 @@ describe('Sign In Tests', () => {
     cy.visit('/signin')
   })
 
-  it('can sign me in with enter', () => {
-    cy.get('.form-input[data-cy=sign-in-email]').type('a@a.com')
-    cy.get('.form-input[data-cy=sign-in-password]').type('Tester1!{enter}')
-    cy.url().should('eq', 'http://localhost:3000/')
-    cy.get('.header').contains('SIGN OUT') 
-  })
-
-  it('signs me out', () => {
+  it('can sign me in and out', () => {
     cy.get('.form-input[data-cy=sign-in-email]').type('a@a.com')
     cy.get('.form-input[data-cy=sign-in-password]').type('Tester1!{enter}')
     cy.url().should('eq', 'http://localhost:3000/')
     cy.get('.header').contains('SIGN OUT') 
     cy.get('.option[data-cy=sign-out-header]').click()
     cy.get('.header').contains('SIGN IN') 
-
-
-    // cy.get('.form-input[data-cy=sign-in-email]').type('a@a.com')
-    // cy.wait(500)
-    // cy.get('.form-input[data-cy=sign-in-password]').type('Tester1!')
-    // cy.wait(500)
-    // cy.get('.custom-button[data-cy=sign-in-button]').click()
-    // cy.get('.option[data-cy=sign-out-header]').click()
-    // cy.wait(500)
-    // cy.get('.header').contains('SIGN IN') 
   })
+
+  // it('signs me out', () => {
+  //   cy.get('.form-input[data-cy=sign-in-email]').type('a@a.com')
+  //   cy.get('.form-input[data-cy=sign-in-password]').type('Tester1!')
+  //   cy.get('.option[data-cy=sign-out-header]').click()
+  //   cy.get('.header').contains('SIGN IN') 
+  // })
 })
