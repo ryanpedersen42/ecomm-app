6621c8c4e615fd3c904b9aade767ae5c263051c3
Ryan Pedersen <ryanpedersen42@gmail.com>
2019-12-09 19:38:36 -0600

    adding redux tests

---
+++ cypress/integration/redux/redux_spec.js
@@ -0,0 +1,58 @@
+import SHOP_DATA from '../../../src/redux/shop/shop.data';
+
+it('has expected state on load', () => {
+  cy.visit('/')
+  cy.window().its('store').invoke('getState').should('deep.equal', {
+      user: {
+        currentUser: null,
+      },
+      cart: {
+        cartItems: [],
+        hidden: true,
+      }, 
+      directory: {
+        sections: [
+          {
+            title: 'electronics',
+            imageUrl: 'https://images.unsplash.com/photo-1513104487127-813ea879b8da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+            id: 1,
+            linkUrl: 'shop/electronics'
+          },
+          {
+            title: 'cameras',
+            imageUrl: 'https://images.unsplash.com/photo-1444069665489-8d498a22e2a6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+            id: 2,
+            linkUrl: 'shop/cameras'
+          },
+          {
+            title: 'accessories',
+            imageUrl: 'https://images.unsplash.com/photo-1541667558913-5510fb3c7bd9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+            id: 3,
+            linkUrl: 'shop/accessories'
+          },
+          {
+            title: 'shoes',
+            imageUrl: 'https://images.unsplash.com/photo-1516478177764-9fe5bd7e9717?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+            size: 'large',
+            id: 4,
+            linkUrl: 'shop/shoes'
+          },
+          {
+            title: 'apparel',
+            imageUrl: 'https://images.unsplash.com/photo-1520006403909-838d6b92c22e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+            size: 'large',
+            id: 5,
+            linkUrl: 'shop/apparel'
+          }
+        ]
+      }, 
+      shop: {
+        collections: SHOP_DATA
+      },
+      _persist: {
+        rehydrated: true,
+        version: -1
+      }
+    // visibilityFilter: 'show_all',
+  })
+})
--- cypress/integration/sign-in/signin_spec.js
+++ cypress/integration/sign-in/signin_spec.js
@@ -4,18 +4,29 @@ describe('Sign In Tests', () => {
   })
 
   it('can sign me in and out', () => {
-    cy.get('.form-input[data-cy=sign-in-email]').type('a@a.com')
-    cy.get('.form-input[data-cy=sign-in-password]').type('Tester1!{enter}')
-    cy.url().should('eq', 'http://localhost:3000/')
-    cy.get('.header').contains('SIGN OUT') 
-    cy.get('.option[data-cy=sign-out-header]').click()
-    cy.get('.header').contains('SIGN IN') 
-  })
+    //longer test to confirm the sign in functionality
+    cy
+      .get('.form-input[data-cy=sign-in-email]')
+      .type('a@a.com')
 
-  // it('signs me out', () => {
-  //   cy.get('.form-input[data-cy=sign-in-email]').type('a@a.com')
-  //   cy.get('.form-input[data-cy=sign-in-password]').type('Tester1!')
-  //   cy.get('.option[data-cy=sign-out-header]').click()
-  //   cy.get('.header').contains('SIGN IN') 
-  // })
+    cy
+      .get('.form-input[data-cy=sign-in-password]')
+      .type('Tester1!{enter}')
+
+    cy
+      .url()
+      .should('eq', 'http://localhost:3000/')
+
+    cy
+      .get('.header')
+      .contains('SIGN OUT') 
+
+    cy
+      .get('.option[data-cy=sign-out-header]')
+      .click()
+
+    cy
+      .get('.header')
+      .contains('SIGN IN') 
+  })
 })
