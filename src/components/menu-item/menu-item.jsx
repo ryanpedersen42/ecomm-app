7873f9bcc2f81a1f8649c8b11d046d6f91243518
Ryan Pedersen <ryanpedersen@Ryans-MBP-2.attlocal.net>
2020-06-01 20:06:25 -0500

    testing for cicd

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -5,15 +5,15 @@ import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
   return (
-    <div className={`${size} menu-item`} 
-    onClick={() => history.push(`${match.url}${linkUrl}`)}
+    <div className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
     >
-    <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}} 
-          />
-          <div className='content'>
-            <h1 className='title' data-cy={`${title}`}>{title.toUpperCase()}</h1>
-              <span className='subtitle'>SHOP NOW</span>
-          </div>
+      <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className='content'>
+        <h1 className='title' data-cy={`${title}`}>{title.toUpperCase()}</h1>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
     </div>
   )
 }
